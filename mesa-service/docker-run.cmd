441aa343da8531925122a805e1c8ee0bc961a5f6
Lisbeth <yoselynespinoza27@gmail.com>
2025-10-29 03:29:05 -0500

    dockerfiles

---
+++ mesa-service/docker-run.cmd
@@ -0,0 +1,3 @@
+cd C:\Users\User\Documents\Ciclo 2025\Desarrollo de Aplicaciones Web II\PROYECTO\dawii-proyecto-dev
+docker build -t restaurant/mesa-service:1.0 -f mesa-service/Dockerfile .
+
---
+++ transporte-service/docker-run.cmd
@@ -0,0 +1,3 @@
+cd C:\Users\User\Documents\Ciclo 2025\Desarrollo de Aplicaciones Web II\PROYECTO\dawii-proyecto-dev
+docker build -t restaurant/transporte-service:1.0 -f transporte-service/Dockerfile .
+
