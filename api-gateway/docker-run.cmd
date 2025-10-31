700349f17d3a3da061aac298f948761eb98b9cb0
Arturo Roncal <roncalhermenegildo@gmail.com>
2025-10-30 21:11:30 -0500

    Update docker-run.cmd

--- api-gateway/docker-run.cmd
+++ api-gateway/docker-run.cmd
@@ -2,4 +2,4 @@ REM Creamos la imagen del servidor eureka
 docker build -t restaurant/api-gateway:1.0 -f Dockerfile .
 
 REM Iniciamos una instancia de la imagen Servidor Eureka
-docker run -d --name api-gateway --network restaurant-net -p 8000:8000 restaurant/api-gateway:1.0
+docker run -d --name api-gateway --network restaurant-net -p 8080:8080 restaurant/api-gateway:1.0
